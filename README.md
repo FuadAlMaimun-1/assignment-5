7b47fbd10cbfbdf98728a6bce90fb9c57403b4ca
FuadAlMaimun-1 <fuadhamja5@gmail.com>
2026-09-12 03:25:52 +0600

    first commit

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 2.Add your favorite technologies to your stack and remove them when needed.
 
 3.Get notifications when adding, removing, or duplicating technologies.
+
+4.Build a personal technology stack easily.
+
+5.Remove all selected technologies with one click.
+
+6.Use a responsive design on desktop, tablet, and mobile devices.
+
+7.View a loading spinner while the technology data is loading.
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,21 +6,19 @@ const Nav = () => {
   const [activeTab, setActiveTab] = useState("signup");
 
   return (
-   <nav className="fixed top-0 inset-x-0 z-50 bg-white border-b border-gray-100 py-3 md:py-4">
-  
+    <nav className="fixed top-0 inset-x-0 z-50 bg-white border-b border-gray-100 py-3 md:py-4">
       <div className="flex justify-between items-center mx-30 gap-2 sm:gap-4">
+        <img
+          src={Hamburger}
+          alt="menu"
+          className="md:hidden w-6 h-6 cursor-pointer"
+        />
 
         <div className="shrink-0">
           <img src={Logo} alt="Logo" />
         </div>
 
         <div>
-          <img
-            src={Hamburger}
-            alt="menu"
-            className="md:hidden w-6 h-6 cursor-pointer"
-          />
-
           <ul className="hidden md:flex gap-6 text-[#475569] cursor-pointer">
             <li className="text-[#DB2777]">Home</li>
             <li>Technologies</li>
@@ -32,7 +30,9 @@ const Nav = () => {
 
         <div className="flex gap-1 sm:gap-2 p-1 rounded-3xl shrink-0">
           <button
-            onClick={() => setActiveTab("signin")} className={`px-4 py-1 sm:py-1.5  rounded-3xl cursor-pointer ${activeTab === "signin"
+            onClick={() => setActiveTab("signin")}
+            className={`px-4 py-1 sm:py-1.5  rounded-3xl cursor-pointer ${
+              activeTab === "signin"
                 ? "bg-[#DB2777] text-white"
                 : "text-gray-600 "
             }`}
